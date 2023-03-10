27dfb73fc42e2bee918033f80f860c9fe637b277
SuperSkyDeveloper <super.skystar92@gmail.com>
2023-03-10 12:23:28 -0500

    initial side bar

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
+import SideBar from './components/side-bar/SideBar';
 
 export default function Home() {
     return (
         <div>
             <Header />
             <h1>Main Content</h1>
+            <div className='main-content'>
+                <SideBar />
+            </div>
         </div>
     );
 
--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
 import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Home from './Home';
 
-function render() {
-  ReactDOM.render(<Home />, document.getElementById('root'));
-}
 
-render();
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<Home />);
