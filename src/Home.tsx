b1f0409dd8b7aa93794dba9a53a46ab5161c3c37
SuperSkyDeveloper <super.skystar92@gmail.com>
2023-03-10 00:13:41 -0500

    tob bar first commit

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Header from './components/Header';
+import Header from './components/header/Header';
 
 export default function Home() {
     return (
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './header.css';
+
+export default function Header() {
+    const headerNavItems = [
+        {
+            label: 'Selected Market',
+            content: 'PIVX/LTC'
+        },
+        {
+            label: 'Latest Trade Price',
+            content: '0.004924 LTC',
+        },
+        {
+            label: '24hr Price',
+            content: '+119.79%',
+        },
+        {
+            label: '24 hr Volume',
+            content: '238.5 LTC',
+        },
+    ];
+
+    const headerTitle = 'BLOCK DX';
+
+    const renderNavItems = () => {
+        return (
+            <ul className="navbar-nav">
+                {
+                    headerNavItems.map(({label, content}) => (
+                        <li className="nav-item nav-item-container d-flex flex-column">
+                            <span className="nav-item-label" aria-current="page">{label}</span>
+                            <span className="nav-item-content">{content}</span>
+                        </li>
+                    ))
+                }
+            </ul>
+        )
+    }
+    return (
+        <nav className="navbar navbar-expand-sm navbar-light bg">
+            <div className="container-fluid">
+                <a className="navbar-brand logo">
+                    <img className='logo-vector' />
+                    {headerTitle}
+                </a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    {renderNavItems()}
+                </div>
+            </div>
+        </nav>
+    );
+
+}
