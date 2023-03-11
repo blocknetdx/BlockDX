241cef8e6d3caf842ca068ffb7bd186ca100c327
SuperSkyDeveloper <super.skystar92@gmail.com>
2023-03-10 23:24:36 -0500

    add font

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import * as ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "@fontsource/ibm-plex-sans";
 import Home from './Home';
 
 function render() {
